97c44bfb6b29074bd1da0e947c946b9e060486e9
Matt Gattis <gattis@gmail.com>
2018-06-13 04:22:11 -0400

    added start of capture stream to alsa

--- alsa/alsa.cc
+++ alsa/alsa.cc
@@ -8,12 +8,36 @@
 #include <alsa/asoundlib.h>
 #include <sys/time.h>
 
-#define ErrorCheck(STMT)					\
+#define QCX(STMT,EXTRA,RET)					\
   if ((err = (STMT)) < 0) {					\
     printf(#STMT " error %d: %s\n", err, snd_strerror(err));	\
-    args.GetReturnValue().Set(Undefined(isolate));		\
-    return;							\
+    EXTRA;							\
+    return RET;							\
   }
+
+#define SETARGS  args.GetReturnValue().Set(Undefined(isolate));
+#define QCA(STMT) QCX(STMT,SETARGS,)
+#define QC(STMT) QCX(STMT,,err)
+
+int pcm_config(snd_pcm_t *pcm, int rate, int period_size, int buffer_size, int thresh) {
+
+  int err;
+  
+  snd_pcm_hw_params_t *hwparams;
+  snd_pcm_hw_params_alloca(&hwparams);
+  QC(snd_pcm_hw_params_any(pcm, hwparams));
+  QC(snd_pcm_hw_params_set_rate_resample(pcm, hwparams, 1));
+  QC(snd_pcm_hw_params_set_access(pcm, hwparams, SND_PCM_ACCESS_MMAP_INTERLEAVED));
+  QC(snd_pcm_hw_params_set_format(pcm, hwparams, SND_PCM_FORMAT_S32));
+  QC(snd_pcm_hw_params_set_channels(pcm, hwparams, 2));
+  QC(snd_pcm_hw_params_set_rate(pcm, hwparams, rate, 0));
+  QC(snd_pcm_hw_params_set_period_size(pcm, hwparams, period_size, 0));
+  QC(snd_pcm_hw_params_set_buffer_size(pcm, hwparams, buffer_size));
+  QC(snd_pcm_hw_params(pcm, hwparams));
+
+  return 0;
+  
+}
 
 namespace alsa {
 
@@ -34,12 +58,12 @@ namespace alsa {
   class Sound : public node::ObjectWrap {
   public:
     static void Init(v8::Local<v8::Object> exports);
-    explicit Sound(snd_pcm_t *handle, snd_output_t *output);
-    snd_pcm_t *handle;
+    explicit Sound(snd_pcm_t *playback, snd_output_t *output);
+    snd_pcm_t *playback, *capture;
     snd_output_t *output;
     int started;
-    snd_pcm_uframes_t viewOffset;
-    snd_pcm_uframes_t viewFrames;
+    snd_pcm_uframes_t viewOffset,viewFrames,periodSize;
+
   private:
     ~Sound();
     static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
@@ -51,11 +75,12 @@ namespace alsa {
   Persistent<Function> Sound::constructor;
 
   Sound::Sound(snd_pcm_t *h, snd_output_t *o) {
-    handle = h;
+    playback = h;
     output = o;
   }
   Sound::~Sound() {
-    snd_pcm_close(handle);
+    snd_pcm_close(playback);
+    snd_pcm_close(capture);
   }
 
   void Sound::Init(Local<Object> exports) {
@@ -85,78 +110,63 @@ namespace alsa {
     }
     
     int err;
-    snd_pcm_t *handle;
+    snd_pcm_t *playback,*capture;
     snd_output_t *output = NULL;
-    snd_pcm_hw_params_t *hwparams;
-    snd_pcm_sw_params_t *swparams;
-
-    char device[50];
-    args[0]->ToString()->WriteUtf8(device);
     int rate = args[1]->IntegerValue();
     int period_size = args[2]->IntegerValue();
     int buffer_size = args[3]->IntegerValue();
     int thresh = (buffer_size / period_size) * period_size;
-
-    ErrorCheck(snd_output_stdio_attach(&output, stdout, 0));
-    ErrorCheck(snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0));
     
-    snd_pcm_hw_params_alloca(&hwparams);
-    ErrorCheck(snd_pcm_hw_params_any(handle, hwparams));
-    ErrorCheck(snd_pcm_hw_params_set_rate_resample(handle, hwparams, 1));
-    ErrorCheck(snd_pcm_hw_params_set_access(handle, hwparams, SND_PCM_ACCESS_MMAP_INTERLEAVED));
-    ErrorCheck(snd_pcm_hw_params_set_format(handle, hwparams, SND_PCM_FORMAT_S32));
-    ErrorCheck(snd_pcm_hw_params_set_channels(handle, hwparams, 2));
-    ErrorCheck(snd_pcm_hw_params_set_rate(handle, hwparams, rate, 0));
-    ErrorCheck(snd_pcm_hw_params_set_period_size(handle, hwparams, period_size, 0));
-    ErrorCheck(snd_pcm_hw_params_set_buffer_size(handle, hwparams, buffer_size));
-    ErrorCheck(snd_pcm_hw_params(handle, hwparams));
-      
-    snd_pcm_sw_params_alloca(&swparams);
-    ErrorCheck(snd_pcm_sw_params_current(handle, swparams));
-    ErrorCheck(snd_pcm_sw_params_set_start_threshold(handle, swparams, thresh));
-    ErrorCheck(snd_pcm_sw_params_set_avail_min(handle, swparams, period_size));
-    ErrorCheck(snd_pcm_sw_params(handle, swparams));
-
-    snd_pcm_dump(handle, output);
-    Sound *obj = new Sound(handle,output);
+    char device[50];
+    args[0]->ToString()->WriteUtf8(device);
+    
+    QCA(snd_output_stdio_attach(&output, stdout, 0));
+    QCA(snd_pcm_open(&playback, device, SND_PCM_STREAM_PLAYBACK, 0));
+    QCA(snd_pcm_open(&capture, device, SND_PCM_STREAM_CAPTURE, 0));
+    QCA(pcm_config(playback, rate, period_size, buffer_size, thresh));
+    QCA(pcm_config(capture, rate, period_size, buffer_size, thresh));
+    
+    snd_pcm_dump(playback, output);
+    snd_pcm_dump(capture, output);
+    
+    Sound *obj = new Sound(playback,output);
+    obj->periodSize = period_size;
     obj->started = 1;
     obj->Wrap(args.This());
     args.GetReturnValue().Set(args.This());
   }
-
-
+  
     
   void Sound::WriteBuffer(const FunctionCallbackInfo<Value>& args) {
     Isolate *isolate = args.GetIsolate();
     Sound* obj = ObjectWrap::Unwrap<Sound>(args.Holder());
     int err;
     const snd_pcm_channel_area_t *areas;
-    snd_pcm_uframes_t offset, frames, period_size, buffer_size;
+    snd_pcm_uframes_t offset, frames;
     snd_pcm_sframes_t avail, target;
-    ErrorCheck(snd_pcm_get_params(obj->handle, &buffer_size, &period_size));
-    target = period_size;
-    snd_pcm_state_t state = snd_pcm_state(obj->handle);
+    target = obj->periodSize;
+    snd_pcm_state_t state = snd_pcm_state(obj->playback);
     if (state == SND_PCM_STATE_XRUN) {
       printf("UNDERRUN\n");
-      ErrorCheck(snd_pcm_prepare(obj->handle));
+      QCA(snd_pcm_prepare(obj->playback));
       obj->started=1;
     } else if (state == SND_PCM_STATE_SUSPENDED) {
       printf("SUSPENDED???\n");
       exit(0);
     }
-    avail = snd_pcm_avail_update(obj->handle);
+    avail = snd_pcm_avail_update(obj->playback);
     while (avail < target) {
-      ErrorCheck(avail);
+      QCA(avail);
       if (obj->started) {
 	obj->started=0;
-	ErrorCheck(snd_pcm_start(obj->handle));
+	QCA(snd_pcm_start(obj->playback));
       } else {
-	ErrorCheck(snd_pcm_wait(obj->handle,1));
+	QCA(snd_pcm_wait(obj->playback,1));
       }
-      avail = snd_pcm_avail_update(obj->handle);
+      avail = snd_pcm_avail_update(obj->playback);
     }
-    frames = (avail / period_size) * period_size;
-    ErrorCheck(snd_pcm_mmap_begin(obj->handle, &areas, &offset, &frames));
+    frames = (avail / obj->periodSize) * obj->periodSize;
+    QCA(snd_pcm_mmap_begin(obj->playback, &areas, &offset, &frames));
     Local<ArrayBuffer> locbuf = ArrayBuffer::New(isolate, areas[0].addr, frames*4*2);
     obj->viewOffset = offset;
     obj->viewFrames = frames;
@@ -168,8 +178,8 @@ namespace alsa {
     Sound* obj = ObjectWrap::Unwrap<Sound>(args.Holder());
     int err;
     snd_pcm_uframes_t frames = obj->viewFrames, offset = obj->viewOffset;
-    snd_pcm_sframes_t ncommit = snd_pcm_mmap_commit(obj->handle, offset, frames);
-    ErrorCheck(ncommit);
+    snd_pcm_sframes_t ncommit = snd_pcm_mmap_commit(obj->playback, offset, frames);
+    QCA(ncommit);
     if ((snd_pcm_uframes_t) ncommit != obj->viewFrames)
       printf("ERROR! committed %ld / %lu frames\n", ncommit, obj->viewFrames);
     args.GetReturnValue().Set(Undefined(isolate));     
