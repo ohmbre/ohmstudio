ef7f9e7115c9cf489fd1929ea80ef4cbbf4dfbd0
Matt Gattis <gattis@gmail.com>
2021-04-24 19:22:55 -0400

    improved clang/llvm integration, now checked

--- external/cxx-jit.h
+++ external/cxx-jit.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include <fstream>
 #include <string>
 #include <vector>
+#include <filesystem>
 
 #include "clang/Basic/Stack.h"
 #include "clang/Basic/TargetOptions.h"
+#include "clang/Driver/Driver.h"
 #include "clang/Config/config.h"
 #include "clang/Driver/DriverDiagnostic.h"
+#include "clang/Driver/Compilation.h"
 #include "clang/Driver/Options.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/CompilerInvocation.h"
@@ -17,6 +21,7 @@
 #include "clang/FrontendTool/Utils.h"
 #include <llvm/ExecutionEngine/Orc/LLJIT.h>
 #include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/Host.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/ADT/SmallString.h>
 #include <llvm/ADT/StringRef.h>
@@ -25,26 +30,6 @@
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/MemoryBuffer.h>
 
-#define STRLIT(A) #A
-#define STRVAR(A) STRLIT(A)
-
-static char ccstr[] = STRVAR(CXXJIT_ARGS) " -emit-llvm-bc";
-static char *ccarg[128];
-int nccarg = []() {
-    int pos = 0;
-    bool quot = false;
-    ccarg[pos++] = ccstr;
-    for (size_t i = 0; i < sizeof(ccstr); i++)
-        if (ccstr[i] == '"') quot = !quot;
-        else if (ccstr[i] == ' ' and !quot) {
-            ccstr[ccstr[i-1] == '"' ? (i-1) : i] = '\0';
-            ccarg[pos++] = ccstr + i + (ccstr[i+1] == '"' ? 2 : 1);
-        }
-    return pos;
-}();
-
-
-static void LLVMErrorHandler(void *UserData, const std::string &Message, bool GenCrashDiag);
 
 class CxxJIT {
     public:
@@ -55,8 +40,13 @@ class CxxJIT {
             ctx = std::make_unique<llvm::LLVMContext>();
             jit = cantFail(llvm::orc::LLJITBuilder().create());
             Clang = std::make_unique<clang::CompilerInstance>();
-            jit->getMainJITDylib().addGenerator(cantFail(
-                llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(jit->getDataLayout().getGlobalPrefix())));
+            jit->getMainJITDylib().addGenerator(cantFail(llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(jit->getDataLayout().getGlobalPrefix())));
+            Clang->createDiagnostics();
+            
+            diagId = new clang::DiagnosticIDs;
+            diagOpts = new clang::DiagnosticOptions;
+            diagBuf = new clang::TextDiagnosticBuffer;
+            diag = new clang::DiagnosticsEngine(diagId, &*diagOpts, diagBuf);
         }
         
         ~CxxJIT() {
@@ -65,28 +55,48 @@ class CxxJIT {
         
         
         bool compile(std::string src) {
-            using llvm::sys::fs::createTemporaryFile;
             int fd;
-            llvm::SmallString<128> name;
-            if (auto ec = createTemporaryFile("cxx-jit", "cpp", fd, name)) {
+            llvm::SmallString<128> llvmcppf;
+            if (auto ec = llvm::sys::fs::createTemporaryFile("cxx-jit", "cpp", fd, llvmcppf)) {
                 errors.push_back(ec.message());
                 return false;
             }
-            
-            constexpr bool shouldClose = true;
-            constexpr bool unbuffered = true;
-            llvm::raw_fd_ostream os(fd, shouldClose, unbuffered);
+            llvm::raw_fd_ostream os(fd, true, true);
             os << src;
-            llvm::StringRef inFileName = name.str();
-            std::string inFname = inFileName.str();
-            std::string bc = name.substr(0, inFileName.find_last_of('.') + 1).str() + "bc";
+            std::string cppf = llvmcppf.str().str();
+            std::string bcf = cppf.substr(0, cppf.find_last_of('.') + 1) + "bc";
+            llvm::SmallVector<const char *, 256> args = {"-emit-llvm","-S","-o", bcf.data(), cppf.data()};
             
-            if (!genByteCode(inFname, name.substr(0, inFileName.find_last_of('.') + 1).str() + "bc")) {
-                errors.push_back("compilation error. skipping linking stage");
+            clang::driver::Driver driver("", llvm::sys::getDefaultTargetTriple(), *diag);
+                        
+            //driver.setInstalledDir(std::filesystem::current_path().string());
+                       
+            std::unique_ptr<clang::driver::Compilation> compilation(driver.BuildCompilation(args));           
+            if (compilation->containsError()) {
+                errors.push_back("error: invocation issue");
                 return false;
             }
             
-            llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> buffer = llvm::MemoryBuffer::getFile(bc);
+            auto &cmd = *compilation->getJobs().begin();
+            auto argv = cmd.getArguments();
+            argv.emplace_back("-emit-llvm-bc");
+            bool success = clang::CompilerInvocation::CreateFromArgs(Clang->getInvocation(), argv, *this->diag);
+            if (!success) errors.push_back("Could not invoke compiler");
+                      
+            Clang->createDiagnostics();
+            
+            this->diagBuf->FlushDiagnostics(Clang->getDiagnostics());
+            if (!success) return false;
+            
+            success = ExecuteCompilerInvocation(Clang.get());
+            this->diagBuf->FlushDiagnostics(Clang->getDiagnostics());
+            if (!success || !std::filesystem::exists(bcf)) {
+                errors.push_back("compilation error. check stdout.");
+                return false;
+            }
+
+                       
+            llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> buffer = llvm::MemoryBuffer::getFile(bcf);
             if (!buffer) {
                 errors.push_back(buffer.getError().message());
                 return false;
@@ -94,19 +104,17 @@ class CxxJIT {
             
             auto module = parseBitcodeFile(buffer.get()->getMemBufferRef(), *ctx);
             
-            llvm::sys::fs::remove(bc);
+            llvm::sys::fs::remove(bcf);
+            llvm::sys::fs::remove(cppf);
             
-            if (!module) {
-                llvm::sys::fs::remove(inFileName.str());
+            if (!module) {    
                 llvm::Error err = module.takeError();
                 llvm::handleAllErrors(std::move(err), [&](const llvm::StringError &strErr) {
                     errors.push_back("error creating module: " + strErr.getMessage());
                 });
                 return false;
             }
-            
-            deleters.push_back([inFname]() { llvm::sys::fs::remove(inFname); });
-            
+                        
             return !jit->addIRModule(llvm::orc::ThreadSafeModule(std::move(*module), std::move(ctx)));
             
         }
@@ -141,11 +149,8 @@ class CxxJIT {
             for (const auto &e : errors) s += e+"\n";
             return s;
         }
-        
-        void handleLLVMErr(std::string msg) {
-            errors.push_back("fatal compiler message: " + msg);
-        }
-        
+               
+        std::vector<const char *> driverArgv;
     private:
         std::unique_ptr<llvm::LLVMContext> ctx;
         std::unique_ptr<llvm::orc::LLJIT> jit;
@@ -153,45 +158,12 @@ class CxxJIT {
         std::vector<std::function<void()>> deleters;
         std::unique_ptr<clang::CompilerInstance> Clang;
         std::vector<std::string> errors;
-        
-        
-        bool genByteCode(std::string inFileName, std::string outFileName) {
-            clang::IntrusiveRefCntPtr<clang::DiagnosticIDs> diagId(new clang::DiagnosticIDs());
-            clang::IntrusiveRefCntPtr<clang::DiagnosticOptions> diagOpts = new clang::DiagnosticOptions();
-            clang::TextDiagnosticBuffer *diagBuf = new clang::TextDiagnosticBuffer;
-            clang::DiagnosticsEngine diag(diagId, &*diagOpts, diagBuf);
-            
-            std::vector<const char *> argv(ccarg, ccarg + nccarg);
-            argv.push_back(inFileName.data());
-            argv.push_back("-o");
-            argv.push_back(outFileName.data());
-            bool Success = clang::CompilerInvocation::CreateFromArgs(Clang->getInvocation(), argv, diag);
-            if (!Success) errors.push_back("Could not invoke compiler");
-            
-            Clang->createDiagnostics();
-            if (!Clang->hasDiagnostics()) {
-                errors.push_back("!Clang->hasDiagnostics");
-                return false;
-            }
-            
-            llvm::install_fatal_error_handler(LLVMErrorHandler, static_cast<void*>(this));
-            diagBuf->FlushDiagnostics(Clang->getDiagnostics());
-            if (!Success) return false;
-            Success = ExecuteCompilerInvocation(Clang.get());
-            llvm::remove_fatal_error_handler();
-            return Success;
-        }
-        
+        clang::IntrusiveRefCntPtr<clang::DiagnosticIDs> diagId;
+        clang::IntrusiveRefCntPtr<clang::DiagnosticOptions> diagOpts;
+        clang::TextDiagnosticBuffer *diagBuf;
+        clang::DiagnosticsEngine *diag;
+                
 };
-
-
-static void LLVMErrorHandler(void *UserData, const std::string &Message, bool GenCrashDiag) {
-    CxxJIT *jit = (CxxJIT*) UserData;
-    jit->handleLLVMErr(Message);
-}   
-
-
-
 
 
 
